63c2cbff35992a615e9b632832988850da87da3b
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-09-20 01:25:17 +0300

    Update socfish.sh

--- socfish.sh
+++ socfish.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# 
+#            --------------------------------------------------
+#                          Social Fisher (SocFish)                
+#            --------------------------------------------------
+#          Copyright (C) <2019>  <Entynetproject (Ivan Nikolsky)>
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#    About Author :   
+#    Founder   : Entynetproject (Ivan Nikolsky)
+#    Site      : http://entynetproject.simplesite.com/
+#    Instagram : @entynetproject 
+#    Twitter   : @entynetproject
+#    Email     : entynetproject@gmail.com
+#
+
 trap 'printf "\n";stop;exit 1' 2
 clear
 
