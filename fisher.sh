9d4be5cdc05d76c66a96ee85182be9c38a88826d
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-09-19 22:06:57 +0300

    Update fisher.sh

--- fisher.sh
+++ fisher.sh
@@ -5,8 +5,8 @@ trap 'printf "\n";stop;exit 1' 2
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
-command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+command -v php > /dev/null 2>&1 || { echo >&2 "Error, php is not installed! Aborting..."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "Error, curl is not installed! Aborting..."; exit 1; }
 
 }
 
@@ -130,7 +130,8 @@ fi
 
 banner() {
 
-
+cd banner
+cat banner.txt
 printf "\n"
 }
 
